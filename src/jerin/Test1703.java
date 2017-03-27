3dc0953b48e2a18636d2d70f5b6b32c81f65f1c3
jchelapurath <jchelapurath@student.tgm.ac.at>
2017-03-27 10:51:10 +0200

    v2

--- src/jerin/Test1703.java
+++ src/jerin/Test1703.java
@@ -4,6 +4,8 @@ public class Test1703 {
 	
 	public static void main(String[] args) {
 		System.out.println("Hallo Jerin");
+		System.out.println("Hallo Jerin");
+		System.out.println("Hallo Jerin");
 	}
 
 }
